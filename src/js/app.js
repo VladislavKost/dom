1842b8ee7968f9a3792e0df166bd91936ec9a3f7
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-08-10 19:58:43 +0500

    [Ref] Move table construction into js code

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ export const updateGoblins = (goblins, imgTag) => {
 };
 
 export const startGame = () => {
+  makeTable();
   const goblins = document.querySelectorAll(".game-field");
   const imgTag = document.createElement("img");
   imgTag.classList.add("goblin");
@@ -15,6 +16,22 @@ export const startGame = () => {
     "https://github.com/netology-code/ahj-homeworks/raw/video/dom/pic/goblin.png";
 
   setInterval(() => updateGoblins(goblins, imgTag), 1000);
+};
+export const makeTable = () => {
+  const body = document.querySelector("body");
+  const tableTag = document.createElement("table");
+  body.appendChild(tableTag);
+  for (let _ of Array(4).keys()) {
+    const trTag = document.createElement("tr");
+    for (let _ of Array(4).keys()) {
+      const tdTag = document.createElement("td");
+      const divTag = document.createElement("div");
+      divTag.classList.add("game-field");
+      tdTag.appendChild(divTag);
+      trTag.appendChild(tdTag);
+    }
+    tableTag.appendChild(trTag);
+  }
 };
 
 document.addEventListener("DOMContentLoaded", startGame);
